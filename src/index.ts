8b26a9364e5b18b3b6374b015575552a2792ad11
Markus Kurzmann <markus@kurzmann.io>
2023-06-03 22:51:12 +0200

    Export LogLevel

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import cheese from "./log/cheese";
 import { CheeseColors } from "./types/CheeseColors";
 import { CheeseConfig } from "./types/CheeseConfig";
 import { ContextDependentCheeseConfig } from "./types/ContextDependentCheeseConfig";
+import { LogLevel } from "./types/LogLevel";
 
 export { cheese, CheeseColors };
-export type { CheeseConfig, ContextDependentCheeseConfig };
+export type { CheeseConfig, ContextDependentCheeseConfig, LogLevel };
 export default cheese;
