98d932cdfe8c125e2c764d2d8e826070968c4b23
Markus Kurzmann <markus@kurzmann.io>
2024-03-30 09:58:59 +0100

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import { Who } from "who-am-i-now";
 import cheese from "./log/cheese";
 import { CheeseColors } from "./types/CheeseColors";
 import { CheeseConfig } from "./types/CheeseConfig";
+import { FormatMessageFn } from "./types/FormatMessageFn";
 import { ContextDependentCheeseConfig } from "./types/ContextDependentCheeseConfig";
 import { LogLevel } from "./types/LogLevel";
 
 export { cheese, CheeseColors, LogLevel };
-export type { CheeseConfig, ContextDependentCheeseConfig };
+export type {
+  CheeseConfig,
+  ContextDependentCheeseConfig,
+  FormatMessageFn,
+  Who,
+};
 export default cheese;
